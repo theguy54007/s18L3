ecdb90c8dbb345fb844f4a708cf0adf4540bb814
Tommy_Tan <sage.ts920126@gmail.com>
2018-02-02 13:39:55 +0800

    setup effect

--- js/index.js
+++ js/index.js
@@ -18,6 +18,30 @@ class BaseCharacter {
     if (this.hp <= 0) {
       this.die();
     }
+
+    var _this = this;
+    var i = 1;
+    _this.id = setInterval(function(){
+      if (i == 1){
+        _this.element.getElementsByClassName("effect-image")[0].style.display = "block";
+        _this.element.getElementsByClassName("hurt-text")[0].classList.add("attacked") ;
+        _this.element.getElementsByClassName("hurt-text")[0].textContent = damage;
+      }
+      _this.element.getElementsByClassName("effect-image")[0].src = "images/effect/blade/" + i + ".png";
+      i ++;
+      if (i > 8){
+        _this.element.getElementsByClassName("effect-image")[0].style.display = "none";
+        _this.element.getElementsByClassName("hurt-text")[0].classList.remove("attacked");
+        _this.element.getElementsByClassName("hurt-text")[0].textContent = "";
+        clearInterval(_this.id);
+      }
+
+
+    },50);
+
+
+
+
   }
   die(){
     this.alive = false;
